400bfd4909dfb8c6e68e4c1c0dfaa8d2e9ba9b3a
Bahri106 <110438429+Bahri106@users.noreply.github.com>
2022-08-07 18:48:40 +0700

    Add files via upload

---
+++ shop/valo.js
@@ -0,0 +1,23 @@
+const valorant = (prefix) => {
+return `*Saiful Bahri*
+		
+PROSES	1-180Mnt kon tol not		
+OPEN	00.00-00.00		
+FORMAT	ID		
+			
+VALORANT POINT			
+		
+125VP	00,000.00		
+420VP	00,000.00		
+700VP	00,000.00		
+1.375VP	000,000.00		
+2.400VP	000,000.00		
+4,000.00VP	000,000.00		
+8.150VP	000,000.00		
+			
+Untuk pay / pembayan silahkan ketik ${prefix}pay
+`
+	}
+
+exports.list_valo = valorant
+ 
