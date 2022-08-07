40373cb569303dc515b94db0cfbba4eed25c4ddc
Bahri106 <110438429+Bahri106@users.noreply.github.com>
2022-08-07 18:53:06 +0700

    Add files via upload

---
+++ shop/sausage.js
@@ -0,0 +1,24 @@
+const sausage = (prefix) => {
+return `*Saiful bahri*
+	
+PROSES	1-60Mnt Max khon tol		
+OPEN	00.00-00.00		
+FORMAT	ID		
+			
+CANDY SAUSAGE			
+	
+60C	Rp 00,000.00		
+180C	Rp 00,000.00		
+316C	Rp 00,000.00		
+718C	Rp 135,000.00		
+1.368C	Rp 000,000.00		
+2.118C	Rp 000,000.00		
+3.548C	Rp 000,000.00		
+7.096C	Rp 0.000,000.00		
+			
+Untuk pay / pembayan silahkan ketik ${prefix}pay
+`
+	}
+
+exports.list_sausage = sausage
+
